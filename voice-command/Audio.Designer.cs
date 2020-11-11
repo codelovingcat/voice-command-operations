55e1670936dece17d244e5fbcde0aba9edb07280
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-11 16:28:40 +0200

    voice-command-audio

---
+++ voice-command/Audio.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace voice_command
+{
+    partial class Audio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnSpeak.Font = new System.Drawing.Font("Minion Pro SmBd", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSpeak.Location = new System.Drawing.Point(12, 79);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(556, 109);
+            this.btnSpeak.TabIndex = 0;
+            this.btnSpeak.UseVisualStyleBackColor = false;
+            this.btnSpeak.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minion Pro SmBd", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(117, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Önce butona tıklayın sonra konuşun";
+            // 
+            // Audio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(580, 245);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSpeak);
+            this.Name = "Audio";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ voice-command/Audio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+namespace voice_command
+{
+    public partial class Audio : Form
+    {
+        public Audio()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpeechRecognitionEngine sr = new SpeechRecognitionEngine();
+            Grammar gramer = new DictationGrammar();
+            sr.LoadGrammar(gramer);
+
+            try
+            {
+                btnSpeak.Text = "Konuşun";
+                sr.SetInputToDefaultAudioDevice();
+                RecognitionResult result = sr.Recognize();
+                btnSpeak.Text = result.Text;
+            }
+            catch (Exception)
+            {
+                btnSpeak.Text = "Hata!";
+            }
+        }
+    }
+}
