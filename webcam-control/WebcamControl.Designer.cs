4bb98a1acba7eff9b9370e84922f5e1c4a679c56
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-11 23:56:14 +0200

    webcam-control

---
+++ webcam-control/WebcamControl.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace webcam_control
+{
+    partial class WebcamControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWebCame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnCatch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnWebCame
+            // 
+            this.btnWebCame.BackColor = System.Drawing.Color.Thistle;
+            this.btnWebCame.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnWebCame.Location = new System.Drawing.Point(355, 20);
+            this.btnWebCame.Name = "btnWebCame";
+            this.btnWebCame.Size = new System.Drawing.Size(98, 33);
+            this.btnWebCame.TabIndex = 0;
+            this.btnWebCame.Text = "Kamera Aç";
+            this.btnWebCame.UseVisualStyleBackColor = false;
+            this.btnWebCame.Click += new System.EventHandler(this.btnWebCame_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minion Pro Med", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kameralar : ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(542, 323);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.comboBox1.Font = new System.Drawing.Font("Minion Pro Med", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(91, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(258, 30);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // btnCatch
+            // 
+            this.btnCatch.BackColor = System.Drawing.Color.Lavender;
+            this.btnCatch.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCatch.Location = new System.Drawing.Point(459, 20);
+            this.btnCatch.Name = "btnCatch";
+            this.btnCatch.Size = new System.Drawing.Size(98, 33);
+            this.btnCatch.TabIndex = 4;
+            this.btnCatch.Text = "Yakala";
+            this.btnCatch.UseVisualStyleBackColor = false;
+            this.btnCatch.Click += new System.EventHandler(this.btnCatch_Click);
+            // 
+            // WebcamControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(696, 397);
+            this.Controls.Add(this.btnCatch);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnWebCame);
+            this.Name = "WebcamControl";
+            this.Text = "WebCame Uygulaması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWebCame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnCatch;
+    }
+}
+
---
+++ webcam-control/WebcamControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace webcam_control
+{
+    public partial class WebcamControl : Form
+    {
+        public WebcamControl()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filterInfo;
+        VideoCaptureDevice videoCapture;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo item in filterInfo)
+            {
+                comboBox1.Items.Add(item.Name);
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void btnWebCame_Click(object sender, EventArgs e)
+        {
+            videoCapture = new VideoCaptureDevice(filterInfo[comboBox1.SelectedIndex].MonikerString);
+            videoCapture.NewFrame += VideoCapture_NewFrame;
+            videoCapture.Start();
+        }
+
+        private void VideoCapture_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void btnCatch_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "(*.jpg)|*.jpg";
+            DialogResult result = saveFile.ShowDialog();
+            if (result==DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFile.FileName);
+            }
+        }
+    }
+}
