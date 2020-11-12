93a486a5a8a8a9bdb15126d0b2a64dd88f3ac856
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-12 16:49:42 +0200

    Ocr-optical-character-recognition

---
+++ Ocr-optical-character-recognition/OpticalCharacterRecognition.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Ocr_optical_character_recognition
+{
+    partial class OpticalCharacterRecognition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OpticalCharacterRecognition));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnText = new System.Windows.Forms.Button();
+            this.btnPdf = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(304, 297);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(324, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(333, 297);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnText
+            // 
+            this.btnText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnText.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnText.ForeColor = System.Drawing.Color.DimGray;
+            this.btnText.Location = new System.Drawing.Point(87, 337);
+            this.btnText.Name = "btnText";
+            this.btnText.Size = new System.Drawing.Size(224, 49);
+            this.btnText.TabIndex = 2;
+            this.btnText.Text = "Resimdeki Yazıyı Aktar";
+            this.btnText.UseVisualStyleBackColor = false;
+            this.btnText.Click += new System.EventHandler(this.btnText_Click);
+            // 
+            // btnPdf
+            // 
+            this.btnPdf.BackColor = System.Drawing.Color.Fuchsia;
+            this.btnPdf.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPdf.ForeColor = System.Drawing.Color.DimGray;
+            this.btnPdf.Location = new System.Drawing.Point(324, 337);
+            this.btnPdf.Name = "btnPdf";
+            this.btnPdf.Size = new System.Drawing.Size(224, 49);
+            this.btnPdf.TabIndex = 3;
+            this.btnPdf.Text = "Yazıyı Pdf e Aktar";
+            this.btnPdf.UseVisualStyleBackColor = false;
+            this.btnPdf.Click += new System.EventHandler(this.btnPdf_Click);
+            // 
+            // OpticalCharacterRecognition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(669, 398);
+            this.Controls.Add(this.btnPdf);
+            this.Controls.Add(this.btnText);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "OpticalCharacterRecognition";
+            this.Text = "Optical cCharacter Recognition";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnText;
+        private System.Windows.Forms.Button btnPdf;
+    }
+}
+
---
+++ Ocr-optical-character-recognition/OpticalCharacterRecognition.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IronOcr;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Ocr_optical_character_recognition
+{
+    public partial class OpticalCharacterRecognition : Form
+    {
+        public OpticalCharacterRecognition()
+        {
+            InitializeComponent();
+        }
+
+        private void btnText_Click(object sender, EventArgs e)
+        {
+            var orc = new AutoOcr();
+            var result = orc.Read(pictureBox1.Image);
+            richTextBox1.Text = result.Text;
+        }
+
+        private void btnPdf_Click(object sender, EventArgs e)
+        {
+            iTextSharp.text.Document document = new iTextSharp.text.Document();
+            PdfWriter.GetInstance(document, new FileStream("D:Document.pdf", FileMode.Create));
+            
+            if (document.IsOpen()== false)
+            {
+                document.Open();
+            }
+            document.Add(new Paragraph(richTextBox1.Text));
+            //document.Close();
+        }
+    }
+}
